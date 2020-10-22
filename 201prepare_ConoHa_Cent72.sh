e67b722b23aa1c13f68fa1e8f37e39cc27f80b2a
dummyotsuka <64056576+dummyotsuka@users.noreply.github.com>
2020-10-22 11:05:59 +0900

    Update 201prepare_ConoHa_Cent72.sh

--- 201prepare_ConoHa_Cent72.sh
+++ 201prepare_ConoHa_Cent72.sh
@@ -24,6 +24,7 @@ yum -y install dump
 rm -f /etc/systemd/system/default.target
 ln -s /usr/lib/systemd/system/graphical.target /etc/systemd/system/default.target
 yum -y install mkisofs
+yum -y install httpd
 
 SetupDisk 0
 SetupDisk 1
